ede6aa44d8202c9b88de1f1956df6d57817e3dee
AdrianGerman <adrian.german1019@gmail.com>
2024-08-31 08:16:14 -0600

    CRUD and http states

--- routes/products.router.js
+++ routes/products.router.js
@@ -21,12 +21,44 @@ router.get('/filter', (req, res) => {
   res.send('Yo soy un filtro');
 });
 
-router.get('/products/:id', (req, res) => {
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  if (id === '999') {
+    res.status(404).json({
+      message: 'not found',
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Product 2',
+      price: 2000,
+    });
+  }
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.status(201).json({
+    message: 'created',
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
   res.json({
+    message: 'deleted',
     id,
-    name: 'Product 2',
-    price: 2000,
   });
 });
 
