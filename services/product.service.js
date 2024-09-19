6371ca1951a069881abd80e8fcfad8d778ba9029
AdrianGerman <adrian.german1019@gmail.com>
2024-09-19 12:12:20 -0600

    filtering prices with operators

--- services/product.service.js
+++ services/product.service.js
@@ -1,6 +1,7 @@
 const { faker } = require('@faker-js/faker');
-const boom = require('@hapi/boom');
+const { Op } = require('sequelize');
 
+const boom = require('@hapi/boom');
 const pool = require('../libs/postgres.pool');
 
 const { models } = require('../libs/sequelize');
@@ -34,11 +35,23 @@ class ProductsService {
   async find(query) {
     const options = {
       include: ['category'],
+      where: {},
     };
     const { limit, offset } = query;
     if (limit && offset) {
       options.limit = limit;
       options.offset = offset;
+    }
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
     }
     const products = await models.Product.findAll(options);
     return products;
