13ed3fd369d5de46e411f66df94dac250d9af78f
AdrianGerman <adrian.german1019@gmail.com>
2024-09-19 09:28:03 -0600

    resolving many-to-many relationships

--- services/order.service.js
+++ services/order.service.js
@@ -8,12 +8,16 @@ class OrderService {
     const newOrder = await models.Order.create(data);
     return newOrder;
   }
+  async addItem(data) {
+    const newItem = await models.OrderProduct.create(data);
+    return newItem;
+  }
   async find() {
     return [];
   }
   async findOne(id) {
     const order = await models.Order.findByPk(id, {
-      include: [{ association: 'customer', include: ['user'] }],
+      include: [{ association: 'customer', include: ['user'] }, 'items'],
     });
     return order;
   }
